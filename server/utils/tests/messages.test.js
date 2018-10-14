d610be5aff99a5c598cc9ab39bae309f95c0bdd0
Lukasz Sobek <lukaszsobek@me.com>
2018-10-14 12:24:13 +0200

    Adds makeLocationLinkMessage test

--- server/utils/tests/messages.test.js
+++ server/utils/tests/messages.test.js
@@ -1,7 +1,11 @@
-const { makeMessage } = require("../messages");
+const {
+    makeLocationLinkMessage,
+    makeMessage
+} = require("../");
 
 describe("Messages", () => {
-    it("makeMessage creates a well-formated message", () => {
+
+    it("makeMessage creates a well-formatted message", () => {
         const from = "Me";
         const text = "test message.";
         const testMessage = makeMessage(from, text);
@@ -9,5 +13,22 @@ describe("Messages", () => {
         expect(testMessage.from).toBe(from);
         expect(testMessage.text).toBe(text);
         expect(typeof testMessage.createdAt).toBe("number");
+    });
+
+    it("makeLocationLinkMessage creates a well-formatted location message", () => {
+        const from = "Me";
+        const location = { latitude: 10, longitude: 20 };
+        const locationMessage = makeLocationLinkMessage(
+            from,
+            location
+        );
+
+        const returnedLink = 
+        "https://www.google.com/maps/place/"
+        + location.latitude
+        + "/" + location.longitude;
+
+        expect(locationMessage.from).toBe(from);
+        expect(locationMessage.locationUrl).toBe(returnedLink);
     });
 });
